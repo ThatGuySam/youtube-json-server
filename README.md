5ddd2de2030fe721b330ae4b0537265b66874dba
Sam Carlton <sam@sam.lc>
2020-02-12 14:57:04 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+[![Deploy to now](https://deploy.now.sh/static/button.svg)](https://deploy.now.sh/?repo=https://github.com/ThatGuySam/zeit-now-stale-example)
+
+# Zeit Now Stale-While-Revalidate Caching Eaxmple
+
+
+A simple example of how to cache requests via Zeit Now to reduce server and database loads while still delivering fast response time by using Stale-While-Revalidate to update the cached data in the background. 
+
+Caches for 5 minutes(set as 300 seconds)
+
+
+[About Stale-While-Revalidate on Zeit](https://zeit.co/docs/v2/network/caching#stale-while-revalidate)
+
+
---
+++ subreddit.js
@@ -0,0 +1,23 @@
+// URL utility
+import url from 'url'
+// The universal version of Fetch so we can use it on both the server and browser
+import 'isomorphic-unfetch'
+
+module.exports = async function (req, res) {
+    const { query: { slug } } = url.parse(req.url, true)
+
+    // Fetch the front page of reddit as JSON
+    const apiResponse = await fetch(`https://www.reddit.com/r/${slug}/top.json`)
+    // Convert data to json
+    const json = await apiResponse.json()
+
+    // Set Cors Headers to allow all origins so data can be requested by a browser
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    // Set JSON Header so requesting applications know how to handle the data
+    res.setHeader('Content-Type', 'application/json')
+
+    // Send the response body as JSON
+    res.json(json)
+}
