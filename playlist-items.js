dae9305c6ad50ea290f9ee9563f3c6b80f6754f2
Sam Carlton <sam@sam.lc>
2020-02-12 17:47:47 -0600

    Setup Youtube playlist-items endpoint

---
+++ playlist-items.js
@@ -0,0 +1,41 @@
+// URL utility
+import url from 'url'
+// Google API (Which contains the Youtube API)
+import { google } from 'googleapis'
+
+module.exports = async function (req, res) {
+    const { query: { id } } = url.parse(req.url, true)
+
+    // Setup Youtube API V3 Service instance
+    const service = google.youtube('v3')
+
+    const { errors = null, data = null } = await service.playlistItems.list({
+        // auth: auth,
+        key: process.env.GOOGLE_API_KEY,
+        part: 'snippet,contentDetails',
+        playlistId: id,
+        // forUsername: 'GoogleDevelopers'
+    }).catch(({ errors }) => {
+
+        console.log('Error fetching playlist', errors)
+
+        return {
+            errors
+        }
+    })
+
+    // Set Cors Headers to allow all origins so data can be requested by a browser
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+
+    // Send an error response if something went wrong
+    if (errors !== null) {
+        res.json({
+            errors: 'Error fetching playlist'
+        })
+
+        return
+    }
+
+    res.json(data.items)
+}
