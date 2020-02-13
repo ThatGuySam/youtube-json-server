e42fded674d8e99984138384549f3a88f158b500
Sam Carlton <sam@sam.lc>
2020-02-12 18:29:24 -0600

    Get all items from playlists

--- playlist-items.js
+++ playlist-items.js
@@ -6,18 +6,17 @@ import { google } from 'googleapis'
 module.exports = async function (req, res) {
     // Break out the id param from our request's query string
     const { query: { id } } = url.parse(req.url, true)
+    const perPage = 50
 
     // Setup Youtube API V3 Service instance
     const service = google.youtube('v3')
 
     // Fetch data from the Youtube API
     const { errors = null, data = null } = await service.playlistItems.list({
-        // auth: auth,
         key: process.env.GOOGLE_API_KEY,
         part: 'snippet,contentDetails',
         playlistId: id,
-        maxResults: 50,
-        // forUsername: 'GoogleDevelopers'
+        maxResults: perPage
     }).catch(({ errors }) => {
 
         console.log('Error fetching playlist', errors)
@@ -40,5 +39,33 @@ module.exports = async function (req, res) {
         return
     }
 
-    res.json(data.items)
+    const items = data.items
+
+    console.log('data.nextPageToken', data.nextPageToken)
+
+    // If there are more results then push them to our playlist
+    if (data.nextPageToken !== null) {
+
+        // Store the token for page #2 into our variable
+        let pageToken = data.nextPageToken
+
+        while (pageToken !== null) {
+            // Fetch data from the Youtube API
+            const youtubePageResponse = await service.playlistItems.list({
+                key: process.env.GOOGLE_API_KEY,
+                part: 'snippet,contentDetails',
+                playlistId: id,
+                maxResults: perPage,
+                pageToken: pageToken
+            })
+
+            // Add the videos from this page on to our total items list
+            youtubePageResponse.data.items.forEach(item => items.push(item))
+
+            // Now that we're done set up the next page token or empty out the pageToken variable so our loop will stop
+            pageToken = ('nextPageToken' in youtubePageResponse.data) ? youtubePageResponse.data.nextPageToken : null
+        }
+    }
+
+    res.json(items)
 }
