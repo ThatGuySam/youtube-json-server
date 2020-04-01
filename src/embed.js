19dd735e15538e82bb3220ac865314aeca5e3e50
Sam Carlton <sam@sam.lc>
2020-03-31 21:12:17 -0500

    Use plain attributes instead of dataset

--- src/embed.js
+++ src/embed.js
@@ -4,6 +4,6 @@ document.write('Write works <br />')
 
 console.log('document.currentScript', document.currentScript)
 
-document.write(`currentScript is ${document.currentScript.src} <br />`)
+document.write(`embedType is ${document.currentScript.embedType} <br />`)
 
-document.write(`Channel ID is ${document.currentScript.dataset.channelId} <br />`)
+document.write(`Channel ID is ${document.currentScript.channelId} <br />`)
