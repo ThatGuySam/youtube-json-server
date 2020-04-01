c17c27c594cc57faa7bf90d7cbe6ab97e93eebfe
Sam Carlton <sam@sam.lc>
2020-03-31 20:45:55 -0500

    Use src and data-channel-id

--- src/embed.js
+++ src/embed.js
@@ -4,4 +4,6 @@ document.write('Write works <br />')
 
 console.log('document.currentScript', document.currentScript)
 
-document.write(`currentScript is ${document.currentScript} <br />`)
+document.write(`currentScript is ${document.currentScript.src} <br />`)
+
+document.write(`Channel ID is ${document.currentScript.dataset.channelId} <br />`)
