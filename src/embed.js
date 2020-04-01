0f3e4f9f440099043dae7181dc5c6a794c4914ea
Sam Carlton <sam@sam.lc>
2020-04-01 09:45:14 -0500

    Add live embed button

--- src/embed.js
+++ src/embed.js
@@ -1,9 +1,63 @@
-console.log('Log Works')
+// console.log('Log Works')
 
-document.write('Write works <br />')
+// document.write('Write works <br />')
 
-console.log('document.currentScript', document.currentScript)
+// console.log('document.currentScript.src', document.currentScript.src)
 
-document.write(`embedType is ${document.currentScript.embedType} <br />`)
+const {
+    embedType,
+    channelId,
+    liveHref = null,
+    liveLabel = 'Join Live'
+} = document.currentScript.dataset
 
-document.write(`Channel ID is ${document.currentScript.channelId} <br />`)
+const elementId = `${embedType}-${channelId}`
+
+document.write(`<div class="yjs-area" id="${elementId}-area"></div>`)
+
+const areaElement = document.getElementById(`${elementId}-area`)
+
+
+const createLiveButton = () => {
+    return `
+        <a
+            id="${elementId}-button"
+            class="yjs-live-button"
+            href="${liveHref}"
+            style="
+                text-decoration: none;
+                color: white;
+                background: red;
+                padding: 0.7rem 2rem;
+                font-weight: bold;
+            "
+        >${liveLabel}</a>
+    `
+}
+
+
+const render = async function () {
+
+    
+    const liveBroadcasts = await fetch(`/channel-broadcasts/${channelId}`)
+        .then((response) => {
+            return response.json()
+        })
+
+    console.log('liveBroadcasts', liveBroadcasts)
+
+    if (liveBroadcasts.length !== 0) {
+        areaElement.innerHTML = createLiveButton()
+    } else {
+        areaElement.innerHTML = ''
+    }    
+}
+
+render()
+
+setInterval(render, 10000)
+
+// const channelId = scriptUrl.searchParams.get('channel-id')
+// const type = scriptUrl.searchParams.get('type')
+
+
