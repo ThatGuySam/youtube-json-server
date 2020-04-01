c87b7c4045e6a5fe1f306ef2cbe37d26c1735913
Sam Carlton <sam@sam.lc>
2020-03-31 19:34:39 -0500

    Log currentScript

--- src/embed.js
+++ src/embed.js
@@ -1,5 +1,7 @@
-console.log('Works')
+console.log('Log Works')
 
 document.write('Write works <br />')
+
+console.log('document.currentScript', document.currentScript)
 
 document.write(`currentScript is ${document.currentScript} <br />`)
