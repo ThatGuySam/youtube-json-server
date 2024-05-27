55404ea5998af01b8a57da554b2c4cd8fd444d71
ThatGuySam <sam@sam.lc>
2024-05-26 21:54:56 -0500

    Log base and api href

--- api/v1/playlist-items.ts
+++ api/v1/playlist-items.ts
@@ -4,16 +4,25 @@
 // 50 second video: http://localhost:3000/672898525.mp4
 
 import 'dotenv/config'
-// import axios from 'axios'
+import process from 'node:process'
+import path from 'node:path'
+import axios from 'axios'
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default async function ( request: VercelRequest, response: VercelResponse ) {
-
     try {
+        if ( !request.url ) {
+            throw new Error( 'No URL' )
+        }
+
+        const { base } = path.parse( request.url )
+        const playlistApiUrl = new URL( `/api/info?query=https://www.youtube.com/playlist?list=PLaQokWZfgbynBbDjr4U1yMqvxOT8LhnPb`, process.env.VERCEL_URL )
         
         return response.json( {
             message: 'Hello from the API',
             path: request.url,
+            api: playlistApiUrl.href,
+            base
         } )
 
         throw new Error( 'Not implemented' )
